758427c24d9a695dd3a21971411ff6a65b180d5c
rickyjulpiter <rickyjulpiter14@gmail.com>
2020-09-25 21:23:48 +0700

    error

---
+++ controller/Sign.php
@@ -0,0 +1,44 @@
+<?php
+
+include_once('Api_request.php');
+class Sign extends Api_request
+{
+    public function __construct()
+    {
+        $api_request = new Api_request();
+        $this->api_request = $api_request;
+    }
+
+    public function sign_up($name, $address, $phone, $email, $password, $password_confirmation)
+    {
+        $name = htmlentities($name);
+        $address = htmlentities($address);
+        $phone = htmlentities($phone);
+        $email = htmlentities($email);
+        $password = htmlentities($password);
+        $password_confirmation = htmlentities($password_confirmation);
+
+        //cek ulang apakah password sudah beneran sama, manatau di client side ada terjadi hijacking code
+        if ($password_confirmation === $password) {
+            $api_endpoint = "signup";
+            $params = "name=" . $name . "&address=" . $address . "&phone=" . $phone . "&email=" . $email . "&password=" . $password . "&password_confirmation=" . $password_confirmation;
+            $api_request = $this->api_request;
+            $request = $api_request->request_post($api_endpoint, $params);
+
+            // $request = json_decode($request, TRUE);
+
+            return $request;
+        }
+
+
+        $api_endpoint = "/product/all";
+        $params = "request_type=1";
+        $api_request = $this->api_request;
+        $request = $api_request->request_post($api_endpoint, $params);
+
+        return array(
+            "status" => $request['status'], //memberitahukan status true/false
+            "result" => json_decode($request['result'], TRUE)
+        );
+    }
+}
---
+++ template/footer.php
@@ -0,0 +1,156 @@
+    <!--footer-start-->
+    <section>
+        <div class="footer-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="footer-logo">
+                            <img src="./assets/images/layout-4/logo/logo.png" class="img-fluid  " alt="logo">
+                        </div>
+                        <div class="logo-detail">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                                has been the industry's standard dummy text ever since the 1500s, when an unknown
+                                printer took a galley of type and scrambled it to make a type specimen book. It has
+                                survived not only five centuries, but also the leap into electronic typesetting, </p>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="follow">
+                            <div class="app-link-block  bg-transparent">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="app-link-bloc-contain p-0">
+                                            <div class="app-item-group">
+                                                <div class="app-item">
+                                                    <img src="./assets/images/layout-1/app/1.png" class="img-fluid"
+                                                        alt="app-banner">
+                                                </div>
+                                                <div class="app-item">
+                                                    <img src="./assets/images/layout-1/app/2.png" class="img-fluid"
+                                                        alt="app-banner">
+                                                </div>
+                                            </div>
+                                            <div class="app-item-group ">
+                                                <div class="sosiyal-block">
+                                                    <h6>follow us</h6>
+                                                    <ul class="sosiyal">
+                                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                                        <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                                        <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="top-category">
+                            <div class="row footer-theme">
+                                <div class="col-md-6 col-lg-3">
+                                    <div class="footer-box">
+                                        <div class="footer-title">
+                                            <h5>top category</h5>
+                                            <div class="line"></div>
+                                        </div>
+                                        <div class="footer-contant">
+                                            <ul>
+                                                <li><a href="#">new fashion</a></li>
+                                                <li><a href="#">smart device</a></li>
+                                                <li><a href="#">wooden furniture</a></li>
+                                                <li><a href="#">golden jewellery</a></li>
+                                                <li><a href="#">digital camera</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-lg-3">
+                                    <div class="footer-box">
+                                        <div class="footer-title">
+                                            <h5>my account</h5>
+                                            <div class="line"></div>
+                                        </div>
+                                        <div class="footer-contant">
+                                            <ul>
+                                                <li><a href="#">about us</a></li>
+                                                <li><a href="#">contact us</a></li>
+                                                <li><a href="#">terms & conditions</a></li>
+                                                <li><a href="#">returns & exchanges</a></li>
+                                                <li><a href="#">shipping & delivery</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-lg-3">
+                                    <div class="footer-box">
+                                        <div class="footer-title">
+                                            <h5>quick link</h5>
+                                            <div class="line"></div>
+                                        </div>
+                                        <div class="footer-contant">
+                                            <ul>
+                                                <li><a href="#">store location</a></li>
+                                                <li><a href="#"> my account</a></li>
+                                                <li><a href="#"> orders tracking</a></li>
+                                                <li><a href="#"> size guide</a></li>
+                                                <li><a href="#">FAQ </a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-lg-3">
+                                    <div class="footer-box footer-contact-box">
+                                        <div class="footer-title">
+                                            <h5>contact us</h5>
+                                            <div class="line"></div>
+                                        </div>
+                                        <div class="footer-contant">
+                                            <ul class="contact-list">
+                                                <li><i class="fa fa-map-marker"></i>big deal store demo
+                                                    store<br>india-<span>3654123</span></li>
+                                                <li><i class="fa fa-phone"></i>call us: <span>123-456-7898</span></li>
+                                                <li><i class="fa fa-envelope-o"></i>email us: support@bigdeal.com</li>
+                                                <li><i class="fa fa-fax"></i>fax <span>123456</span></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="sub-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-6 col-md-8 col-sm-12">
+                            <div class="footer-end">
+                                <p><span>2020</span> Copy Right by PT. PadiMall Ina</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-md-4 col-sm-12">
+                            <div class="payment-card-bottom">
+                                <ul>
+                                    <li><a href="#"><img src="./assets/images/layout-1/pay/1.png" class="img-fluid"
+                                                alt="pay"></a></li>
+                                    <li><a href="#"><img src="./assets/images/layout-1/pay/2.png" class="img-fluid"
+                                                alt="pay"></a></li>
+                                    <li><a href="#"><img src="./assets/images/layout-1/pay/3.png" class="img-fluid"
+                                                alt="pay"></a></li>
+                                    <li><a href="#"><img src="./assets/images/layout-1/pay/4.png" class="img-fluid"
+                                                alt="pay"></a></li>
+                                    <li><a href="#"><img src="./assets/images/layout-1/pay/5.png" class="img-fluid"
+                                                alt="pay"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--footer-end-->
