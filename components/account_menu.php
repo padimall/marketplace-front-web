acc1c40affb1bc9a8290967d964093c59ed7b817
rickyjulpiter <rickyjulpiter14@gmail.com>
2020-10-05 00:58:29 +0700

    add view of profile

---
+++ components/account_menu.php
@@ -0,0 +1,18 @@
+<div class="col-lg-3">
+    <div class="account-sidebar rounded"><a class="popup-btn">Informasi Akun</a></div>
+    <div class="dashboard-left">
+        <div class="collection-mobile-back">
+            <span class="filter-back"><i class="fa fa-angle-left" aria-hidden="true"></i> back</span>
+        </div>
+        <div class="block-content ">
+            <ul>
+                <li><a href="#">Informasi Akun</a></li>
+                <li><a href="#">Pesanan Saya</a></li>
+                <li><a href="#">Riwayat Pembelian</a></li>
+                <li><a href="#">Toko Saya</a></li>
+                <li><a href="#">Ubah Password</a></li>
+                <li class="last"><a href="#">Keluar</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
---
+++ profile.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('template/head.php');
+if (isset($_POST['btn-profile'])) {
+    $name = $_POST['name'];
+    $address = $_POST['address'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+
+    $profile_update_api = Requests::post($api_endpoint . "user", $header);
+}
+
+$profile_api = Requests::post($api_endpoint . "user", $header);
+$profile_api_status = $profile_api->success;
+$profile_api_data = json_decode($profile_api->body, TRUE);
+
+if ($profile_api_status) {
+    $profile_api_data_name = $profile_api_data['name'];
+    $profile_api_data_email = $profile_api_data['email'];
+    $profile_api_data_address = $profile_api_data['address'];
+    $profile_api_data_phone = $profile_api_data['phone'];
+}
+
+?>
+
+<body class="bg-light ">
+
+    <!-- loader start -->
+    <div class="loader-wrapper">
+        <div>
+            <img src="./assets/images/loader.gif" alt="loader">
+        </div>
+    </div>
+    <!-- loader end -->
+
+    <!--header start-->
+    <?php include('template/header.php') ?>
+    <!--header end-->
+
+    <!-- section start -->
+    <section class="section-big-py-space bg-light">
+        <div class="container">
+            <div class="row">
+                <?php include('components/account_menu.php') ?>
+                <div class="col-lg-9">
+                    <div class="dashboard-right">
+                        <div class="dashboard">
+                            <h3 class="mb-3">Informasi Akun</h3>
+                            <form class="theme-form" method="POST">
+                                <div class="form-row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="name">Nama</label>
+                                            <input type="text" name="nama" class="form-control" id="name"
+                                                placeholder="Nama" required="" value="<?= $profile_api_data_name ?>">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="phone">No. Handphone</label>
+                                            <input type="text" name="phone" class="form-control" id="phone"
+                                                placeholder="No. Handphone" required=""
+                                                value="<?= $profile_api_data_phone ?>">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <div class="form-group">
+                                                <label for="email">Email</label>
+                                                <input type="text" name="email" class="form-control" id="email"
+                                                    placeholder="Email" required=""
+                                                    value="<?= $profile_api_data_email ?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="address">Alamat Lengkap</label>
+                                            <input type="text" name="address" class="form-control" id="address"
+                                                placeholder="Alamat lengkap" required=""
+                                                value="<?= $profile_api_data_address ?>">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <button name="btn-profile"
+                                            class="btn btn-rounded black-btn float-right">Perbaharui Data</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- section end -->
+
+    <!--footer-start-->
+    <?php include('./template/footer.php') ?>
+    <!--footer-end-->
+
+    <!-- tap to top -->
+    <div class="tap-top">
+        <div>
+            <i class="fa fa-angle-double-up"></i>
+        </div>
+    </div>
+
+    <script>
+    function AvoidSpace(event) {
+        var k = event ? event.which : window.event.keyCode;
+        if (k == 32) return false;
+    }
+    </script>
+    <!-- tap to top End -->
+    <?php include('template/script.php') ?>
+
+</body>
+
+</html>
