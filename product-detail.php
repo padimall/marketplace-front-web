8b1aaf22dae67fefcf403f3b5f724750a93fcc90
rickyjulpiter <rickyjulpiter14@gmail.com>
2020-10-04 19:52:24 +0700

    product detail done

--- product-detail.php
+++ product-detail.php
@@ -16,6 +16,7 @@ $product_detail_description = $product_detail_data['description'];
 $product_detail_weight = $product_detail_data['weight'];
 $product_detail_stock = $product_detail_data['stock'];
 $product_detail_min_order = $product_detail_data['min_order'];
+$product_detail_image = $product_detail_data['image'];
 ?>
 
 <body class="bg-light ">
@@ -40,32 +41,54 @@ $product_detail_min_order = $product_detail_data['min_order'];
         <div class="collection-wrapper mb-4">
             <div class="custom-container">
                 <div class="row">
+                    <?php
+                    if (is_null($product_detail_image)) {
+                    ?>
                     <div class="col-lg-4">
                         <div class="product-slick no-arrow">
-                            <div><img src="./assets/images/product-sidebar/001.jpg" alt=""
-                                    class="img-fluid  image_zoom_cls-0"></div>
-                            <div><img src="./assets/images/product-sidebar/002.jpg" alt=""
-                                    class="img-fluid  image_zoom_cls-1"></div>
-                            <div><img src="./assets/images/product-sidebar/003.jpg" alt=""
-                                    class="img-fluid  image_zoom_cls-2"></div>
-                            <div><img src="./assets/images/product-sidebar/004.jpg" alt=""
-                                    class="img-fluid  image_zoom_cls-3"></div>
+                            <div>
+                                <img src="./assets/images/layout-4/product/1.jpg" alt=""
+                                    class="img-fluid image_zoom_cls-0">
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                    } else {
+                    ?>
+                    <div class="col-lg-4">
+                        <div class="product-slick no-arrow">
+                            <?php
+                                foreach ($product_detail_image as $product_detail_image_view) {
+                                    $product_detail_image_view_url = $product_detail_image_view['url'];
+                                    $product_detail_image_view_id = $product_detail_image_view['id'];
+                                ?>
+                            <div>
+                                <img src="<?= $api_image . $product_detail_image_view_url ?>" alt=""
+                                    class="img-fluid  image_zoom_cls-<?= $product_detail_image_view_id ?>">
+                            </div>
+                            <?php } ?>
                         </div>
                         <div class="row">
                             <div class="col-12 p-0">
                                 <div class="slider-nav">
-                                    <div><img src="./assets/images/product-sidebar/001.jpg" alt=""
-                                            class="img-fluid  image_zoom_cls-0"></div>
-                                    <div><img src="./assets/images/product-sidebar/002.jpg" alt=""
-                                            class="img-fluid  image_zoom_cls-1"></div>
-                                    <div><img src="./assets/images/product-sidebar/003.jpg" alt=""
-                                            class="img-fluid  image_zoom_cls-2"></div>
-                                    <div><img src="./assets/images/product-sidebar/004.jpg" alt=""
-                                            class="img-fluid  image_zoom_cls-3"></div>
+                                    <?php
+                                        foreach ($product_detail_image as $product_detail_image_view) {
+                                            $product_detail_image_view_url = $product_detail_image_view['url'];
+                                            $product_detail_image_view_id = $product_detail_image_view['id'];
+                                        ?>
+                                    <div>
+                                        <img src="<?= $api_image . $product_detail_image_view_url ?>" alt=""
+                                            class="img-fluid  image_zoom_cls-<?= $product_detail_image_view_id ?>">
+                                    </div>
+                                    <?php } ?>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <?php
+                    }
+                    ?>
+
                     <div class="col-lg-8 rtl-text">
                         <div class="product-right">
                             <h2><?= $product_detail_name ?></h2>
@@ -79,32 +102,6 @@ $product_detail_min_order = $product_detail_data['min_order'];
                                 <h6 class="product-title">Deskripsi Produk</h6>
                                 <p class="text-justify"><?= $product_detail_description ?></p>
                             </div>
-                            <div class="border-product">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <h6 class="product-title">Info Produk</h6>
-                                    </div>
-                                    <div class="col-md-12 mt-2">
-                                        <div>
-                                            <div class="row">
-                                                <div class="col-md-4">
-                                                    <p class="float-left font-weight-bold">Berat </p>
-                                                    <p class="float-right"><?= $product_detail_weight ?>Kg</p>
-                                                </div>
-                                                <div class="col-md-4">
-                                                    <p class="float-left font-weight-bold">Stock </p>
-                                                    <p class="float-right"><?= $product_detail_stock ?>Kg</p>
-                                                </div>
-                                                <div class="col-md-4">
-                                                    <p class="float-left font-weight-bold">Minimal Pemesanan </p>
-                                                    <p class="float-right"><?= $product_detail_min_order ?>Kg</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
                             <div class="border-product">
                                 <div class="row">
                                     <div class="col-md-12">
