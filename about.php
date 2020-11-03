7fd9503ac00fde2d1d464d0f0ef50feacf3410bb
rickyjulpiter <rickyjulpiter14@gmail.com>
2020-11-03 18:31:51 +0700

    add about page padimall

--- about.php
+++ about.php
@@ -20,6 +20,14 @@ searchProduct();
     <?php
     include('template/header.php');
     ?>
+
+    <style>
+    .li-about {
+        display: list-item;
+        text-align: justify;
+        font-size: calc(14px + (16 - 14) * ((100vw - 320px) / (1920 - 320)));
+    }
+    </style>
     <!--header end-->
 
     <!-- about section start -->
@@ -29,26 +37,44 @@ searchProduct();
                 <div class="col-lg-4">
                     <div class="banner-section">
                         <img src="https://padimall.id/old/img/ainun.png" class="img-fluid w-auto" alt="">
+                        <h3 class="text-center mt-3">Hajah Nurainun Syahril</h3>
+                        <p class="text-center mt-2 mb-5">CEO of PADIMALL</p>
                     </div>
                 </div>
                 <div class="col-lg-8">
-                    <h4>CEO Padi Mall
+                    <h4>CEO PADI MALL
                     </h4>
-                    <p class="mb-2"> Sed ut perspiciatis unde omnis iste natus <b>error sit</b> voluptatem accusantium
-                        doloremque laudantium,</p>
-                    <p class="mb-2"> On the other hand, we denounce with righteous indignation and dislike men who are
-                        so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that
-                        they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to
-                        those who fail in their duty through weakness of will, which is the same as saying through
-                        shrinking from toil and pain.</p>
-                    <p class="mb-2"> These <b>cases</b> are perfectly simple and easy to distinguish. In a free hour,
-                        when our power of choice is untrammelled and when nothing prevents our</p>
-                    <p> being able to do what we like best, every pleasure is to be <b>welcomed</b> and every pain
-                        avoided. But in certain circumstances and owing to the claims of duty or the obligations of
-                        business it will frequently occur that pleasures have to be repudiated and annoyances accepted.
-                        The wise man therefore always holds in these matters to this principle of selection: he rejects
-                        pleasures to secure other greater pleasures, or else he endures pains to avoid <b>worse</b>
-                        pains.</p>
+                    <p class="mb-2 text-justify"> Padimall adalah platform pemasaran (marketplace) yang menjembatani dan
+                        membimbing
+                        pelaku usaha kecil dan menengah dalam merintis, menjalankan dan memasarkan produk-produk hasil
+                        produksi UMKM baik secara lokal maupun international.</p>
+                    <p class="mb-2 text-justify"> Padimall juga beroperasi dengan platform online store, marketplace
+                        berbasis
+                        Website dan Aplikasi Mobile. Sebagai market place bagi produk UKM dan komoditi eksport,
+                        beroperasi di berbagai jaringan dan wilayah Indonesia dengan kemitraan 550 kota dan kabupaten
+                        serta memasarkan 1 juta produk UKM. Menjadi Mitra BumDes/Industri rumahan (Home Industry)/
+                        Pesantren di desa dan kecamatan. Menyediakan layanan pengiriman berdasarkan kesiapan produsen
+                        dalam jangka waktu 3-7 hari waktu eksor.</p>
+
+                    <h4>Visi</h4>
+                    <p class="mb-2 text-justify"> Meningkatkan perekonomian Negara secara digital baik lokal maupun
+                        international melalui industri Usaha Kecil dan Menengah (UKM).</p>
+
+                    <h4>Misi</h4>
+                    <ol style="list-style-type:decimal;padding-left:10px">
+                        <li class="li-about">Meningkatkan industri usaha kecil dan menengah secara lokal dan
+                            international dengan
+                            memasarkan produk- produk unggulan secara digital.</li>
+                        <li class="li-about">Meningkatkan kualitas produk dengan kreativitas dan inovasi dalam
+                            menciptakan produk baru
+                            yang berpotensi dan berdaya saing di pasar international.</li>
+                        <li class="li-about">Meningkatkan evolusi teknologi "supply chain" dengan negara-negara yang
+                            berpotensi sebagai
+                            tujuan ekspor.</li>
+                        <li class="li-about">Menjadi Hub Ekspor produk unggulan.</li>
+                        <li class="li-about">Membangun Network Komunitas ekspor.</li>
+                        <li class="li-about">Menjadi pusat kewirausahaan sosial (Social Enterpreneur).</li>
+                    </ol>
                 </div>
             </div>
         </div>
